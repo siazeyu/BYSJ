606400c662b840b2e0b9e23a7f619fbe057c902e
Siaze <siaze@qq.com>
2023-04-25 17:55:14 +0800

    init

---
+++ boot-credit/src/main/java/com/boot/credit/domain/CreditStatue.java
@@ -0,0 +1,7 @@
+package com.boot.credit.domain;
+
+public class CreditStatue {
+    public static final long APPLYING = 0;
+    public static final long  SUCCESS = 1;
+    public static final long FAIL = 2;
+}
